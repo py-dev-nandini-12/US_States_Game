01ae033b7b7aa3365fe71c064d9ab4147cd93934
Nandini Chatterjee <phy_nandini@icloud.com>
2023-08-22 09:56:31 +0100

    Identify states in US

--- main.py
+++ main.py
@@ -16,16 +16,18 @@ while len(guessed_state) < 50:
     answer_state = screen.textinput(title= f"{len(guessed_state)}/50 States correct" , prompt= "What's the next state? ").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_state:
-                missing_states.append(state)
+    #     missing_states = []
+    #     for state in all_states:
+    #         if state not in guessed_state:
+    #             missing_states.append(state)
+        missing_states = [ state for state in all_states if state not in guessed_state]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
 
     if answer_state in all_states:
         guessed_state.append(answer_state)
+
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
